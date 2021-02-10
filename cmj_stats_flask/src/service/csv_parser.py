d918f8b29cf59e35d9e7a90fa16421f9a1f75808
skarpecki <skarpecki@users.noreply.github.com>
2021-02-10 20:24:16 +0100

    Functions to sort athletes

--- cmj_stats_flask/src/service/csv_parser.py
+++ cmj_stats_flask/src/service/csv_parser.py
@@ -16,62 +16,60 @@ def check_extension(filename):
     return filename.rsplit('.', 1)[1].lower() == "csv"
 
 
-# TODO: There must be more efficient way to do this
+def get_lastname(filename: str):
+    return filename.split("-")[0].split("_")[1]
+
+
+def get_firstname(filename: str):
+    return filename.split("-")[0].split("_")[0]
+
+
+def get_date(filename: str):
+    """us date format"""
+    date_str = filename.split("-")[1].split(".")[0].split("_")
+    return date(int(date_str[2]), int(date_str[0]), int(date_str[1]))
+
+
+# TODO: There must be more efficient way than bubble sort
 
 def sort_list(list_athletes):
     athletes = deepcopy(list_athletes)
     n = len(athletes)
     for i in range(n):
         for j in range(n - 1 - i):
-            temp1 = athletes[j].split("-")
-            temp2 = athletes[j + 1].split("-")
+            temp1 = athletes[j]
+            temp2 = athletes[j + 1]
 
-            temp1_surname = temp1[0].split("_")[1]
-            temp2_surname = temp2[0].split("_")[1]
-
-            if temp1_surname > temp2_surname:
+            if get_lastname(temp1) > get_lastname(temp2):
                 athletes[j], athletes[j + 1] = athletes[j + 1], athletes[j]
 
     for i in range(n):
         for j in range(n - 1 - i):
-            temp1 = athletes[j].split("-")
-            temp2 = athletes[j + 1].split("-")
-            temp1_surname = temp1[0].split("_")[1]
-            temp2_surname = temp2[0].split("_")[1]
-
-            temp1_name = temp1[0].split("_")[0]
-            temp2_name = temp2[0].split("_")[0]
-
-            if temp1_surname == temp2_surname and temp1_name > temp2_name:
+            temp1 = athletes[j]
+            temp2 = athletes[j + 1]
+            if get_lastname(temp1) == get_lastname(temp2) \
+                    and get_firstname(temp1) > get_firstname(temp2):
                 athletes[j], athletes[j + 1] = athletes[j + 1], athletes[j]
 
     for i in range(n):
         for j in range(n - 1 - i):
-            temp1 = athletes[j].split("-")
-            temp2 = athletes[j + 1].split("-")
-            temp1_surname = temp1[0].split("_")[1]
-            temp2_surname = temp2[0].split("_")[1]
-            temp1_name = temp1[0].split("_")[0]
-            temp2_name = temp2[0].split("_")[0]
+            temp1 = athletes[j]
+            temp2 = athletes[j + 1]
 
-            temp1_date_str = temp1[1].split(".")[0].split("_")
-            temp2_date_str = temp2[1].split(".")[0].split("_")
-
-            temp1_date = date(int(temp1_date_str[2]), int(temp1_date_str[0]), int(temp1_date_str[1])) # us date format...
-            temp2_date = date(int(temp2_date_str[2]), int(temp2_date_str[0]), int(temp2_date_str[1]))
-
-            if temp1_surname == temp2_surname and temp1_name == temp2_name and temp1_date > temp2_date:
+            if get_lastname(temp1) == get_lastname(temp2) \
+                    and get_firstname(temp1) == get_firstname(temp1) \
+                    and get_date(temp1) > get_date(temp2):
                 athletes[j], athletes[j + 1] = athletes[j + 1], athletes[j]
     return athletes
 
 
 if __name__ == "__main__":
-    athletess = ["Robin_Volkmar-20_01_2021",
-                 "Robin_Volkmar-20_12_2020",
-                 "Robin_Volkmar-20_11_2020",
-                 "Robin_Volkmar-31_12_2020",
-                 "Szymon_Karpecki-20_12_2020",
-                 "Bartlomiej_Karpecki-20_12_2020",
+    athletess = ["Robin_Volkmar-01_20_2021",
+                 "Robin_Volkmar-12_20_2020",
+                 "Robin_Volkmar-11_20_2020",
+                 "Robin_Volkmar-12_31_2020",
+                 "Szymon_Karpecki-12_20_2020",
+                 "Bartlomiej_Karpecki-12_20_2020",
                  "Adam_Lewandowski-01_01_2021"]
     from pprint import pprint as pp
 
