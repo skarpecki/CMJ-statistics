3c2f4f18e5cd09ceed8cf9cecbc05f835b1217f6
skarpecki <skarpecki@users.noreply.github.com>
2021-02-24 19:29:37 +0100

    Removing files after computations

--- CMJStatsComputer/blueprints/compute_bp.py
+++ CMJStatsComputer/blueprints/compute_bp.py
@@ -9,13 +9,17 @@ bp = Blueprint('cmj-compute', __name__, url_prefix='/cmj/compute')
 
 def compute_thread(filenames: list, vel_file_path: str, force_file_path: str):
     for filename in filenames:
-        with open(r"{}\{}".format(vel_file_path, filename)) as vel_csv_file:
+        vel_path = r"{}\{}".format(vel_file_path, filename)
+        force_path = r"{}\{}".format(force_file_path, filename)
+        with open(vel_path) as vel_csv_file:
             cmj_vel_attr = VelocityCMJAttribute(vel_csv_file)
-        with open(r"{}\{}".format(force_file_path, filename)) as force_csv_file:
+        with open(force_path) as force_csv_file:
             cmj_force_attr = ForceCMJAttribute(force_csv_file)
         cmj = CMJForceVelStats(cmj_vel_attr, cmj_force_attr, "Time (s)")
         stats = cmj.get_cmj_stats()
         print(stats)
+        os.remove(vel_path)
+        os.remove(force_path)
 
 
 @bp.route("/", methods=['POST'])
